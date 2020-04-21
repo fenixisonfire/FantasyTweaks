7fcae06f11afd8a23b15b909e32840552afb58e1
Pilau <bizarrebeyondbelief@gmail.com>
2020-04-21 09:12:43 +0100

    Initial commit

---
+++ FantasyTweaks/Main.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using HarmonyLib;
+using TaleWorlds.MountAndBlade;
+using TaleWorlds.Core;
+
+namespace FantasyTweaks
+{
+    internal class Main : MBSubModuleBase
+    {
+        protected override void OnSubModuleLoad()
+        {
+            base.OnSubModuleLoad();
+
+            var harmony = new Harmony("fantasytweaks.harmony.patch");
+            harmony.PatchAll();
+
+            InformationManager.DisplayMessage(new InformationMessage(""));
+        }
+    }
+
+}
---
+++ FantasyTweaks/Patches.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using HarmonyLib;
+using TaleWorlds.CampaignSystem.SandBox.GameComponents;
+
+namespace FantasyTweaks
+{
+    internal class Patches
+    {
+
+        [HarmonyPatch(typeof(DefaultCharacterDevelopmentModel), "get_LevelsPerAttributePoint")]
+        public class GetLevelsPerAttributePointPatch
+        {
+
+            private static void Postfix(ref int __result)
+            {
+                __result = 1;
+            }
+
+        }
+        
+        [HarmonyPatch(typeof(DefaultCharacterDevelopmentModel), "get_FocusPointsPerLevel")]
+        public class GetFocusPointsPerLevelPatch
+        {
+
+            private static void Postfix(ref int __result)
+            {
+                __result = 2;
+            }
+
+        }
+
+    }
+
+}
