35015179f07ef9567d10a306129aed8565209c7f
Pilau <bizarrebeyondbelief@gmail.com>
2020-04-22 19:16:27 +0100

    Add hourly crafting stamina tick

--- FantasyTweaks/Patches/SmithingPatch.cs
+++ FantasyTweaks/Patches/SmithingPatch.cs
@@ -1,12 +1,15 @@
 ﻿using System;
 using HarmonyLib;
+using TaleWorlds.CampaignSystem;
 using TaleWorlds.CampaignSystem.SandBox.GameComponents.Map;
+using TaleWorlds.CampaignSystem.SandBox.CampaignBehaviors;
 
 namespace FantasyTweaks.Patches
 {
     public class SmithingPatch
     {
         private static readonly Double SMITHING_STAMINA_MULTIPLIER = 0.25;
+        private static readonly int SMITHING_STAMINA_HOURLY_REGAIN = 10;
 
         [HarmonyPatch(typeof(DefaultSmithingModel), "GetEnergyCostForRefining")]
         public class GetEnergyCostForRefiningPatch
@@ -34,5 +37,33 @@ namespace FantasyTweaks.Patches
                 __result = (int)(SMITHING_STAMINA_MULTIPLIER * __result);
             }
         }
+
+        [HarmonyPatch(typeof(CraftingCampaignBehavior), "HourlyTick")]
+        public class HourlyTickPatch
+        {
+
+            private static bool Prefix(CraftingCampaignBehavior __instance)
+            {
+                Hero hero = PartyBase.MainParty.LeaderHero;
+
+                int heroCraftingStamina = __instance.GetHeroCraftingStamina(hero);
+                int maxCraftingStamina = __instance.GetMaxHeroCraftingStamina(hero);
+                if (heroCraftingStamina < maxCraftingStamina)
+                {
+                    int gain = SMITHING_STAMINA_HOURLY_REGAIN;
+                    int fromMax = maxCraftingStamina - heroCraftingStamina;
+                    if (fromMax < gain)
+                    {
+                        gain = fromMax;
+                    }
+                    
+                    __instance.SetHeroCraftingStamina(hero, Math.Min(maxCraftingStamina, heroCraftingStamina + gain));
+
+                }
+
+                return false;
+            }
+        }
+
     }
 }
