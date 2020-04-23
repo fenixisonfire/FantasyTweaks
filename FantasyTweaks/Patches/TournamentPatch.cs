1f156a122d439c8b82ad07db8fae35d0e602206c
Pilau <bizarrebeyondbelief@gmail.com>
2020-04-23 08:31:22 +0100

    Fix tournament win reward

--- FantasyTweaks/Patches/TournamentPatch.cs
+++ FantasyTweaks/Patches/TournamentPatch.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using HarmonyLib;
+﻿using HarmonyLib;
 using TaleWorlds.CampaignSystem.SandBox.GameComponents;
 using SandBox.TournamentMissions.Missions;
 
@@ -7,21 +6,25 @@ namespace FantasyTweaks.Patches
 {
     public class TournamentPatch
     {
+        private static readonly int TOURNAMENT_RENOWN_MULTIPLIER = 2;
+        private static readonly int TOURNAMENT_WIN_REWARD = 1000;
+
         [HarmonyPatch(typeof(DefaultTournamentModel), "GetRenownReward")]
         public class GetRenownRewardPatch
         {
             private static void Postfix(ref int __result)
             {
-                __result = __result * 2;
+                __result *= TOURNAMENT_RENOWN_MULTIPLIER;
             }
         }
 
-        [HarmonyPatch(typeof(TournamentBehavior), "get_OverallExpectedDenars")]
-        public class GetOverallExpectedDenarsPatch
+        [HarmonyPatch(typeof(TournamentBehavior), "OnPlayerWinTournament")]
+        public class OnPlayWinTournamentPatch
         {
-            private static void Postfix(ref int __result)
+            private static bool Prefix(TournamentBehavior __instance)
             {
-                __result = __result + 1000;
+                typeof(TournamentBehavior).GetProperty("OverallExpectedDenars").SetValue(__instance, __instance.OverallExpectedDenars + TOURNAMENT_WIN_REWARD);
+                return true;
             }
         }
     }
