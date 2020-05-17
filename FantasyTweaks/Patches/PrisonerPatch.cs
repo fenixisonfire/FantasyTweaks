b1bae8ed43baa76093a679284ba56f4c18503250
Pilau <bizarrebeyondbelief@gmail.com>
2020-05-17 04:37:22 +0100

    Make all prisoners recruitable after 1 day

--- FantasyTweaks/Patches/PrisonerPatch.cs
+++ FantasyTweaks/Patches/PrisonerPatch.cs
@@ -1,13 +1,13 @@
 ﻿using HarmonyLib;
 using TaleWorlds.CampaignSystem;
-using TaleWorlds.CampaignSystem.SandBox.GameComponents;
-using TaleWorlds.Core;
+using TaleWorlds.CampaignSystem.SandBox.CampaignBehaviors;
 
 namespace FantasyTweaks.Patches
 {
     public class PrisonerPatch
     {
-        [HarmonyPatch(typeof(DefaultPrisonerRecruitmentCalculationModel), "GetDailyRecruitedPrisoners")]
+        // Not needed as DailyTickPatch below is pathing all prisoners to be recruitable after 1 tick
+        // [HarmonyPatch(typeof(DefaultPrisonerRecruitmentCalculationModel), "GetDailyRecruitedPrisoners")]
         public class GetDailyRecruitedPrisonersPatch
         {
             /*
@@ -18,7 +18,7 @@ namespace FantasyTweaks.Patches
              * 
              * The position of the value in the array represents the tier of unit it's for, i.e 0th element for tier 0, 1st element for tier 1, etc.
             */
-            private static readonly float[] RECRUITMENT_PROBABILITY = new float[7] { 1F, 5F, 4F, 3F, 2F, 1F, 1F };
+            private static readonly float[] RECRUITMENT_PROBABILITY = new float[7] { 10F, 10F, 10F, 10F, 10F, 10F, 10F };
 
             public static bool Prefix(ref float[] __result, MobileParty mainParty)
             {
@@ -31,5 +31,23 @@ namespace FantasyTweaks.Patches
                 return true;
             }
         }
+
+        [HarmonyPatch(typeof(RecruitPrisonersCampaignBehavior), "DailyTick")]
+        public class DailyTickPatch
+        {
+            private static bool Prefix(RecruitPrisonersCampaignBehavior __instance)
+            {
+                TroopRoster prisonRoster = MobileParty.MainParty.PrisonRoster;
+                for (int i = 0; i < prisonRoster.Count; i++)
+                {
+                    CharacterObject character = prisonRoster.GetCharacterAtIndex(i);
+                    int troopCount = prisonRoster.GetTroopCount(character);
+                    __instance.SetRecruitableNumber(character, troopCount);
+                }
+
+                return false;
+            }
+        }
+       
     }
 }
