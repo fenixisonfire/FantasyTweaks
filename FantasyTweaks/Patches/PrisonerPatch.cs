916bbc7bdd76f88e9b3bd3abed0a2c5cbe61e479
Pilau <bizarrebeyondbelief@gmail.com>
2020-05-17 05:42:04 +0100

    Tweak prisoner conversion rate

--- FantasyTweaks/Patches/PrisonerPatch.cs
+++ FantasyTweaks/Patches/PrisonerPatch.cs
@@ -1,4 +1,6 @@
-﻿using HarmonyLib;
+﻿using System;
+using HarmonyLib;
+using TaleWorlds.Core;
 using TaleWorlds.CampaignSystem;
 using TaleWorlds.CampaignSystem.SandBox.CampaignBehaviors;
 
@@ -18,7 +20,7 @@ namespace FantasyTweaks.Patches
              * 
              * The position of the value in the array represents the tier of unit it's for, i.e 0th element for tier 0, 1st element for tier 1, etc.
             */
-            private static readonly float[] RECRUITMENT_PROBABILITY = new float[7] { 10F, 10F, 10F, 10F, 10F, 10F, 10F };
+            private static readonly float[] RECRUITMENT_PROBABILITY = new float[7] { 1f, 0.9f, 0.8f, 0.7f, 0.6f, 0.5f, 0.4f };
 
             public static bool Prefix(ref float[] __result, MobileParty mainParty)
             {
@@ -35,14 +37,18 @@ namespace FantasyTweaks.Patches
         [HarmonyPatch(typeof(RecruitPrisonersCampaignBehavior), "DailyTick")]
         public class DailyTickPatch
         {
+            private static readonly float CONVERSION_RATE_MIN = 0.4f;
+
             private static bool Prefix(RecruitPrisonersCampaignBehavior __instance)
             {
                 TroopRoster prisonRoster = MobileParty.MainParty.PrisonRoster;
                 for (int i = 0; i < prisonRoster.Count; i++)
                 {
                     CharacterObject character = prisonRoster.GetCharacterAtIndex(i);
-                    int troopCount = prisonRoster.GetTroopCount(character);
-                    __instance.SetRecruitableNumber(character, troopCount);
+                    int currentlyRecruitable = __instance.GetRecruitableNumber(character);
+                    int totalTroopCount = prisonRoster.GetTroopCount(character);
+                    int newRecruitable = currentlyRecruitable + (int) Math.Ceiling(MBRandom.RandomFloatRanged(CONVERSION_RATE_MIN, 1.0f) * (totalTroopCount - currentlyRecruitable));
+                    __instance.SetRecruitableNumber(character, newRecruitable);
                 }
 
                 return false;
