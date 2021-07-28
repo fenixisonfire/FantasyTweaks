becb77f44f18258a2f5c411d68173f8dfb4fc89a
Pilau <bizarrebeyondbelief@gmail.com>
2021-07-28 20:03:09 +0100

    Fix hero patch

--- FantasyTweaks/Patches/HeroPatch.cs
+++ FantasyTweaks/Patches/HeroPatch.cs
@@ -4,24 +4,19 @@ using TaleWorlds.CampaignSystem;
 
 namespace FantasyTweaks.Patches
 {
+    
+    [HarmonyPatch(typeof(Hero))]
     class HeroPatch
     {
         private static readonly float SKILL_XP_MULTIPLIER = 5.0f;
 
-        [HarmonyPatch(typeof(Hero))]
-        public class AddSkillXpPatch
+        [HarmonyPrefix]
+        [HarmonyPatch("AddSkillXp")]
+        static void AddSkillXpPrefix(Hero __instance, SkillObject skill, ref float xpAmount)
         {
-            [HarmonyPatch("AddSkillXp")]
-            public static void Prefix(Hero __instance, SkillObject skill, ref float xpAmount)
+            if (__instance != null && skill != null && __instance.HeroDeveloper != null && skill.GetName() != null && Hero.MainHero != null)
             {
-                if (shouldApplySkillMultiplier(__instance, skill))
-                {
-                    xpAmount *= SKILL_XP_MULTIPLIER;
-                }
-            }
-            private static bool shouldApplySkillMultiplier(Hero __instance, SkillObject skill)
-            {
-                return __instance != null && skill != null && __instance.HeroDeveloper != null && skill.GetName() != null && Hero.MainHero != null;
+                xpAmount *= SKILL_XP_MULTIPLIER;
             }
         }
     }
