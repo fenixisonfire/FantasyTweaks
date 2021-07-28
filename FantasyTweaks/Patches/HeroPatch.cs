b23d78b61e4689f9c3551ff5c82158a29679b39b
Pilau <bizarrebeyondbelief@gmail.com>
2021-07-28 04:01:41 +0100

    Update loot patch

--- FantasyTweaks/Patches/HeroPatch.cs
+++ FantasyTweaks/Patches/HeroPatch.cs
@@ -9,7 +9,7 @@ namespace FantasyTweaks.Patches
         private static readonly float SKILL_XP_MULTIPLIER = 5.0f;
 
         [HarmonyPatch(typeof(Hero))]
-        public class Patches
+        public class AddSkillXpPatch
         {
             [HarmonyPatch("AddSkillXp")]
             public static void Prefix(Hero __instance, SkillObject skill, ref float xpAmount)
--- FantasyTweaks/Patches/LootPatch.cs
+++ FantasyTweaks/Patches/LootPatch.cs
@@ -4,32 +4,29 @@ using TaleWorlds.Core;
 
 namespace FantasyTweaks.Patches
 {
-    public class LootPatch
+    
+    [HarmonyPatch(typeof(PartyScreenLogic), "ExecuteTroop")]
+    public class ExecuteTroopPatch
     {
-        [HarmonyPatch(typeof(PartyScreenLogic), "ExecuteTroop")]
-        public class ExecuteTroopPatch
+        public static void Postfix(PartyScreenLogic.PartyCommand command)
         {
-            public static void Postfix(PartyScreenLogic.PartyCommand command)
-            {
-                CharacterObject character = command.Character;
+            CharacterObject character = command.Character;
 
-                Equipment battleEquipment = character.HeroObject.BattleEquipment;
+            Equipment battleEquipment = character.HeroObject.BattleEquipment;
                 
-                for (int i = 0; i < Equipment.EquipmentSlotLength - 1; i++)
+            for (int i = 0; i < Equipment.EquipmentSlotLength - 1; i++)
+            {
+                ItemObject item = battleEquipment[i].Item;
+                if (item != null)
                 {
-                    ItemObject item = battleEquipment[i].Item;
-                    if (item != null)
-                    {
-                        PartyBase.MainParty.ItemRoster.AddToCounts(item, 1);
-                        InformationManager.DisplayMessage(
-                            new InformationMessage($"Obtained {item.Name.ToString()}")
-                        );
-                    }
+                    PartyBase.MainParty.ItemRoster.AddToCounts(item, 1);
+                    InformationManager.DisplayMessage(
+                        new InformationMessage($"Obtained {item.Name}")
+                    );
                 }
-
             }
 
         }
- 
+
     }
 }
