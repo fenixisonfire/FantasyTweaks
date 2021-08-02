2463391a49af75de276d5e786fb2e89df014d850
Pilau <bizarrebeyondbelief@gmail.com>
2021-08-02 23:08:50 +0100

    Update version log

--- FantasyTweaks/Main.cs
+++ FantasyTweaks/Main.cs
@@ -32,7 +32,7 @@ namespace FantasyTweaks
         protected override void OnBeforeInitialModuleScreenSetAsRoot()
         {
             base.OnBeforeInitialModuleScreenSetAsRoot();
-            InformationManager.DisplayMessage(new InformationMessage("Fantasy Tweaks 1.6"));
+            InformationManager.DisplayMessage(new InformationMessage("Fantasy Tweaks 1.6.1"));
         }
 
         protected override void OnGameStart(Game game, IGameStarter gameStarterObject)
