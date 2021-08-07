21715f3ed5d88e08167151317adcea4e170adca2
Pilau <bizarrebeyondbelief@gmail.com>
2021-08-08 00:00:03 +0100

    Add item donation patch

---
+++ FantasyTweaks/Patches/DefaultItemDiscardModelPatch.cs
@@ -0,0 +1,19 @@
+﻿using HarmonyLib;
+using TaleWorlds.CampaignSystem.SandBox.GameComponents;
+using TaleWorlds.Core;
+
+namespace FantasyTweaks.Patches
+{
+
+    [HarmonyPatch(typeof(DefaultItemDiscardModel))]
+    public class DefaultItemDiscardModelPatch
+    {
+
+        [HarmonyPostfix]
+        [HarmonyPatch("GetXpBonusForDiscardingItem")]
+        static void GetXpBonusForDiscardingItemPostfix(ItemObject item, ref int __result, int amount = 1)
+        {
+            __result = __result + item.Value * amount;
+        }
+    }
+}
