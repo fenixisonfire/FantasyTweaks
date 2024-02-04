8b78881bc59ed345c582e4cd8799d53b10e0bbab
fenixisonfire <fenixisonfire@users.noreply.github.com>
2024-02-04 01:56:21 +0000

    Add persuasion pre-reject patch

--- FantasyTweaks/Patches/SilverTongue.cs
+++ FantasyTweaks/Patches/SilverTongue.cs
@@ -1,4 +1,5 @@
 ﻿using HarmonyLib;
+using TaleWorlds.CampaignSystem.CampaignBehaviors;
 using TaleWorlds.CampaignSystem.GameComponents;
 
 namespace FantasyTweaks.Patches
@@ -10,6 +11,15 @@ namespace FantasyTweaks.Patches
         static void Postfix(ref float __result)
         {
             __result = 80f;
+        }
+    }
+
+    [HarmonyPatch(typeof(LordDefectionCampaignBehavior), "conversation_lord_persuade_option_reaction_pre_reject_on_condition")]
+    internal class PersuasionPreRejectPatch
+    {
+        public static void Postfix(ref bool __result)
+        {
+            __result = false;
         }
     }
 }
