301677c7dc7702d5fcff1e373ae2b4c160f33c20
fenixisonfire <fenixisonfire@users.noreply.github.com>
2024-02-04 02:19:23 +0000

    Add party size patch

---
+++ FantasyTweaks/Patches/PartyPatch.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using HarmonyLib;
+using TaleWorlds.CampaignSystem.GameComponents;
+using TaleWorlds.CampaignSystem.Party;
+using TaleWorlds.CampaignSystem;
+using TaleWorlds.Localization;
+using TaleWorlds.Core;
+using TaleWorlds.CampaignSystem.CharacterDevelopment;
+
+namespace FantasyTweaks.Patches
+{
+    [HarmonyPatch(typeof(DefaultPartySizeLimitModel), "GetPartyMemberSizeLimit")]
+    internal class PartySizePatch
+    {
+
+        private static readonly TextObject _textCustodianGuard = new TextObject("{=SBIh8N1p}Custodian Guard");
+        
+        public static void Postfix(PartyBase party, bool includeDescriptions, ref ExplainedNumber __result)
+        {
+            if (!party.MobileParty.IsMainParty)
+            {
+                return;
+            }
+            
+            Hero hero = party.LeaderHero;
+            bool hasUltimateLeaderPerk = hero.GetPerkValue(DefaultPerks.Leadership.UltimateLeader);
+            int leadershipSkill = hero.GetSkillValue(DefaultSkills.Leadership);
+            if (hasUltimateLeaderPerk)
+            {
+                int bonus = Math.Max(leadershipSkill - 250, 0);
+                __result.AddFactor(bonus, _textCustodianGuard);
+            }
+        }
+    }
+}
